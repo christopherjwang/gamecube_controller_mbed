9ef8e82bdc72b0dcb64f3c1036f1ded744b2fc4d
Christopher Wang <christopher.j.wang@gmail.com>
2015-12-04 13:53:40 -0500

    follows sending protocol used by controller for high and low bits

--- my_asm.s
+++ my_asm.s
@@ -1,47 +1,7 @@
-/*
     AREA asm_func, CODE, READONLY
     EXPORT my_asm
 my_asm
-    PUSH    {R4, R5}
-//; Load GPIO Port 1 base address in register R1 
-    LDR     R5, =0x2009C000 ; 0x2009C020 = GPIO port 1 base address
-; Move bit mask in register R2 for bit 18 only
-    MOV.W   R2, #0x00000001   ;
-    
-//    MOV.W   R3, #0x00000002   ;
-//    LDR     R0, [R1, #0x14];
-//    AND     R0, R0, R3      ;
-//    LDR R0, =0x1;
-    
-LOOP    
-    SUB     R1, R1, #0x01
-    LDR     R3, [R0, R1]
-    LDR     R3, [R3, #0x01]
-    
-; value passed from C compiler code is in R0 - compare to a "0" 
-    CMP     R3, #0          ; value == 0 ?
-; (If-Then-Else) on next two instructions using equal cond from the zero flag
-    ITE EQ
-; STORE if EQ - clear led 1 port bit using GPIO FIOCLR register and mask
-    STREQ   R2, [R5,#0x1C]
-; STORE if NE - set led 1 port bit using GPIO FIOSET register and mask
-    STRNE   R2, [R5,#0x18]
-
-    
-    CMP     R1, #0
-    BLT     FINISH
-    B      LOOP;
-    
-FINISH
-    POP     {R4, R5}
-    BX      LR
-    END
-*/
- 
-    AREA asm_func, CODE, READONLY
-    EXPORT my_asm
-my_asm
-    PUSH    {R4, R5, R6}
+    PUSH    {R4, R5, R6, LR}
     SUB     R5, R5, R5
     
 LOOP
@@ -54,18 +14,69 @@ LOOP
     LDR     R3, [R0, R5]
     
     CMP     R3, #0          ; value == 0 ?
-    ITE EQ
-    STREQ   R2, [R4,#0x1C]  ; if==0, clear LED1 bit
-    STRNE   R2, [R4,#0x18]  ; if==1, set LED1 bit
+    BNE     SEND_HIGH
+    STR     R2, [R4,#0x1C]  ; set low
+    BL       WAIT_ONE_US_MINUS_OVERHEAD
+    BL       WAIT_ONE_US
+    BL       WAIT_ONE_US
+      ; if==1, set LED1 bit ;cw: "send low"
+    B       _END_SEND_HIGH
+SEND_HIGH
+    STR     R2, [R4,#0x1C]  ; set low
+    BL       WAIT_ONE_US
+    STR     R2, [R4,#0x18]  ; set high
+    BL       WAIT_ONE_US_MINUS_OVERHEAD
+    BL       WAIT_ONE_US
     
-    SUB     R1, R1, #0x01
-    ADD     R5, R5, #0x04
+_END_SEND_HIGH
+    STR     R2, [R4,#0x18]
+    BL       WAIT_ONE_US
+
+
+BACK
+    
+    SUB     R1, R1, #0x01 ; going through the array backwards (it was reversed in the c layer)
+    ADD     R5, R5, #0x04 ; increment address that we access using R5
     
     CMP     R1, #0
     BEQ     FINISH
     B       LOOP
     
 FINISH
-    POP     {R4, R5, R6}
+    POP     {R4, R5, R6, LR}
     BX      LR
+    
+WAIT_ONE_US_MINUS_OVERHEAD
+    LDR     R6, =0x8
+_WAIT_ONE_US_MINUS_OVERHEAD
+    SUB     R6, R6, #1
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    CMP     R6, #0
+    BNE     _WAIT_ONE_US_MINUS_OVERHEAD
+    BX      LR
+    
+WAIT_ONE_US
+    LDR     R6, =0x9
+_WAIT_ONE_US
+    SUB     R6, R6, #1
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    CMP     R6, #0
+    BNE     _WAIT_ONE_US
+    BX      LR
+
+    
+
+    
+    
+    
+    
     END
